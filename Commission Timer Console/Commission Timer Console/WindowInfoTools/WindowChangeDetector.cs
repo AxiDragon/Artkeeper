bb2e0b0c1854d894c8d87053574bc7d862ecda5f
Axi <axidreamdragon@gmail.com>
2023-11-19 13:29:55 +0100

    Base Program
    Starting out!

---
+++ Commission Timer Console/Commission Timer Console/WindowInfoTools/WindowChangeDetector.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+internal class WindowChangeDetector
+{
+    //TODO: makes this static
+    private const int nChars = 256;
+    private bool active = true;
+
+    private string currentWindowName = string.Empty;
+
+    public string CurrentWindowName
+    {
+        get => currentWindowName;
+        set
+        {
+            if (currentWindowName != value)
+            {
+                currentWindowName = value;
+                OnWindowTitleChanged?.Invoke(value);
+
+                Console.WriteLine("New class: " + CurrentWindowName);
+            }
+        }
+    }
+
+    public event Action<string> OnWindowTitleChanged;
+
+    [DllImport("user32.dll")]
+    public static extern int GetForegroundWindow();
+
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+    [DllImport("user32.dll")]
+    public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+    public WindowChangeDetector(Action<string> action)
+    {
+        OnWindowTitleChanged += action;
+
+        DetectActiveWindowChange();
+    }
+
+    private void DetectActiveWindowChange()
+    {
+        while (active)
+        {
+            IntPtr handle = (IntPtr)GetForegroundWindow();
+            StringBuilder className = new StringBuilder(nChars);
+            StringBuilder windowTitle = new StringBuilder(nChars);
+
+            if (GetClassName(handle, className, nChars) > 0)
+            {
+                CurrentWindowName = className.ToString();
+            }
+
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Commission Timer Console/Commission Timer Console/WindowTimer.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+
+internal class WindowTimer
+{
+    private Stopwatch stopwatch = new Stopwatch();
+    private string windowClassToCheckFor = string.Empty;
+
+    public WindowTimer()
+    {
+        string windowTitle = GetWindowTitleToCheckFor();
+
+
+
+        WindowChangeDetector windowChangeDetector = new WindowChangeDetector(OnWindowTitleChanged);
+    }
+
+    private void OnWindowTitleChanged(string windowTitle)
+    {
+        if (windowTitle == windowClassToCheckFor)
+        {
+            stopwatch.Start();
+        }
+        else
+        {
+            stopwatch.Stop();
+        }
+    }
+
+    private string GetWindowTitleToCheckFor()
+    {
+        Console.WriteLine("Select the window you want to track: ");
+        List<string> windowTitles = GetWindowTitles();
+
+        for (int i = 0; i < windowTitles.Count; i++)
+        {
+            Console.WriteLine(i + ": " + windowTitles[i]);
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Enter the number of the window you want to track: ");
+            string? input = Console.ReadLine();
+
+            if (input == null)
+            {
+                Console.WriteLine("Invalid input!");
+                continue;
+            }
+
+            bool gotIndex = int.TryParse(input, out int index);
+
+            if (gotIndex && index >= 0 && index < windowTitles.Count)
+            {
+                return windowTitles[index];
+            }
+
+            Console.WriteLine("Invalid input!");
+        }
+    }
+
+    private List<string> GetWindowTitles()
+    {
+        List<string> windowTitles = new List<string>();
+
+        Process[] processlist = Process.GetProcesses();
+
+        foreach (Process process in processlist)
+        {
+            if (!string.IsNullOrEmpty(process.MainWindowTitle))
+            {
+                windowTitles.Add(process.MainWindowTitle);
+            }
+        }
+
+        return windowTitles;
+    }
+}
