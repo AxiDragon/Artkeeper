c0bfac505dc74f143eb413aa04d05fef3ff38c47
Axi <axidreamdragon@gmail.com>
2023-12-10 00:14:25 +0100

    Add Add Timer Button

--- Artkeeper WPF/Artkeeper WPF/MainWindow.xaml.cs
+++ Artkeeper WPF/Artkeeper WPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Artkeeper.UserControls;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Artkeeper
 {
@@ -7,9 +9,21 @@ namespace Artkeeper
     /// </summary>
     public partial class MainWindow : Window
     {
+        private StackPanel timerStackPanel;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timerStackPanel = (StackPanel)FindName("TimerStackPanel");
+        }
+
+        public void AddTimer_Click(object sender, RoutedEventArgs e)
+        {
+            TimerControl timerControl = new TimerControl();
+
+            int index = timerStackPanel.Children.Count - 1;
+            timerStackPanel.Children.Insert(index, timerControl);
         }
     }
 }
